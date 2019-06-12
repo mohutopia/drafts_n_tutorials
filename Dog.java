f7772a503566324a289ddf7ceb4ecb1421e31903
Moh <32433254+mohutopia@users.noreply.github.com>
2019-06-12 18:20:16 +0100

    easy first steps tutorials in java and c++

---
+++ Dog.java
@@ -0,0 +1,26 @@
+import Animal.Animal;
+
+class Dog extends Animal {
+  int age;
+  
+	public Dog(int dogsAge) {
+    age = dogsAge;
+  }
+	public void bark() {
+    System.out.println("Woof!");
+  }
+  public void run(int feet) {
+    System.out.println("Your dog ran " + feet + " feet!");
+  }
+  public int getAge() {
+    return age;
+  }
+	public static void main(String[] args) {
+		Dog spike = new Dog(22);
+    spike.bark();
+    spike.run(20);
+    int spikeAge = spike.getAge();
+    System.out.println(spikeAge);
+    spike.checkStatus();
+	}
+}
